79d94c3a3b28235d30e80801d7e253a9ab0df877
Paola Garay <garay.angela@gmail.com>
2020-12-21 23:14:45 -0500

    Add Pin content, state and delete

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -4,18 +4,18 @@ export default function reducer(state, { type, payload }){
             return {
                 ...state,
                 currentUser: payload
-            }
+            };
         case "IS_LOGGED_IN":
             return {
                 ...state,
                 isAuth: payload
-            }
+            };
         case "SIGNOUT_USER":
             return {
                 ...state,
                 isAuth: false,
                 currentUser: null
-            }
+            };
         case "CREATE_DRAFT":
             return {
                 ...state,
@@ -23,15 +23,18 @@ export default function reducer(state, { type, payload }){
                     latitude: 0,
                     longitude: 0
                 }
-            }
+            };
         case "UPDATE_DRAFT_LOCATION":
             return {
                 ...state,
                 draft: payload
-            }
+            };
+        case "DELETE_DRAFT":
+            return {
+                ...state,
+                draft: null
+                };
         default:
             return state;
     }
 }
-
-
