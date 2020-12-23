f56e6493e006d5f8c712dc8ea2505517cf7c07c3
Paola Garay <garay.angela@gmail.com>
2020-12-23 11:11:34 -0500

    Create new user pins

--- typeDefs.js
+++ typeDefs.js
@@ -1,32 +1,53 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 module.exports = gql`
   type User {
-      _id: ID
-      name: String
-      email: String
-      picture: String
+    _id: ID
+    name: String
+    email: String
+    picture: String
   }
 
   type Pin {
-      _id: ID
-      createdAt: String
-      title: String
-      content: String
-      image: String
-      latitud: Float
-      longitude: Float
-      author: User
-      comments: [Comment]
+    _id: ID
+    createdAt: String
+    title: String
+    content: String
+    image: String
+    latitude: Float
+    longitude: Float
+    author: User
+    comments: [Comment]
   }
 
   type Comment {
-      text: String
-      createdAt: String
-      author: User
+    text: String
+    createdAt: String
+    author: User
+  }
+
+  input CreatePinInput {
+    title: String
+    image: String
+    content: String
+    latitude: Float
+    longitude: Float
   }
 
   type Query {
-      me: User
+    me: User
+    getPins: [Pin!]
   }
-`
+
+  type Mutation {
+    createPin(input: CreatePinInput!): Pin
+    deletePin(pinId: ID!): Pin
+    createComment(pinId: ID!, text: String!): Pin
+  }
+
+  type Subscription {
+    pinAdded: Pin
+    pinDeleted: Pin
+    pinUpdated: Pin
+  }
+`;
